22bdb1ac13297aeb61e7cada0b35efacc22cab30
Ingo Ruhnke <grumbel@gmx.de>
2010-05-26 13:29:33 +0200

    Copyright date update

--- src/command_line_options.cpp
+++ src/command_line_options.cpp
@@ -589,7 +589,7 @@ CommandLineOptions::print_version() const
 {
   std::cout
     << "xboxdrv 0.4.14\n"
-    << "Copyright (C) 2008 Ingo Ruhnke <grumbel@gmx.de>\n"
+    << "Copyright (C) 2008-2010 Ingo Ruhnke <grumbel@gmx.de>\n"
     << "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n"
     << "This is free software: you are free to change and redistribute it.\n"
     << "There is NO WARRANTY, to the extent permitted by law."
