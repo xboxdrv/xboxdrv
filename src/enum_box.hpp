2054df5c2c3677627793b40ab129a381097c38ff
Ingo Ruhnke <grumbel@gmx.de>
2011-01-20 00:14:44 +0100

    Added iterators to EnumBox

--- src/enum_box.hpp
+++ src/enum_box.hpp
@@ -21,6 +21,7 @@
 
 #include <map>
 #include <string>
+#include <sstream>
 
 template<class Enum>
 class EnumBox
@@ -47,6 +48,15 @@ protected:
   }
 
 public:
+  typedef typename std::map<Enum, std::string>::iterator iterator;
+  typedef typename std::map<Enum, std::string>::iterator const_iterator;
+  
+  const_iterator begin() const { return m_enum2string.begin(); }
+  const_iterator end()   const { return m_enum2string.end();   }
+
+  iterator begin() { return m_enum2string.begin(); }
+  iterator end()   { return m_enum2string.end();   }
+
   Enum operator[](const std::string& str) const 
   {
     typename std::map<std::string, Enum>::const_iterator i = m_string2enum.find(str);
