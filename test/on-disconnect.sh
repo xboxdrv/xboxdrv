18d752dbca98aae7bf75eb9600230d0aa0393ee1
Ingo Ruhnke <grumbel@gmx.de>
2011-01-29 05:12:19 +0100

    Added test script for on-connect/on-disconnect

---
+++ test/on-disconnect.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+exec notify-send  "Xboxdrv" "Controller disconnected ${RANDOM} $*" -t 1000
+
+# EOF #
+
---
+++ test/xboxdrv-daemon-connect-scripts.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+exec ./xboxdrv --daemon \
+  --on-connect test/on-connect.sh \
+  --on-disconnect test/on-disconnect.sh \
+  --next-controller --next-controller
+
+# EOF #
