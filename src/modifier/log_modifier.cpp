2073b9c42fe1e1a51c4f789309c6021eae4a740c
Ingo Ruhnke <grumbel@gmail.com>
2011-07-17 02:42:29 +0200

    Added primitive log-modifier that writes log output of the current msg state

---
+++ src/modifier/log_modifier.cpp
@@ -0,0 +1,65 @@
+/*
+**  Xbox360 USB Gamepad Userspace Driver
+**  Copyright (C) 2011 Ingo Ruhnke <grumbel@gmx.de>
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "modifier/log_modifier.hpp"
+
+#include <iostream>
+#include <sstream>
+
+LogModifier*
+LogModifier::from_string(const std::string& value)
+{
+  return new LogModifier(value);
+}
+
+LogModifier::LogModifier(const std::string& prefix) :
+  m_prefix(prefix)
+{
+}
+
+void
+LogModifier::update(int msec_delta, ControllerMessage& msg)
+{
+  std::cout << m_prefix << ": ";
+  for(int i = 1; i < XBOX_BTN_MAX; ++i)
+  {
+    std::cout << msg.get_button(static_cast<XboxButton>(i));
+    if (i != XBOX_BTN_MAX - 1)
+      std::cout << " ";
+  }
+
+  std::cout << "  ";
+
+  for(int i = 1; i < XBOX_AXIS_MAX; ++i)
+  {
+    std::cout << msg.get_axis(static_cast<XboxAxis>(i));
+    if (i != XBOX_AXIS_MAX - 1)
+      std::cout << " ";
+  }
+  std::cout << std::endl;
+}
+
+std::string
+LogModifier::str() const
+{
+  std::ostringstream os;
+  os << "log:" << m_prefix;
+  return os.str();
+}
+
+/* EOF */
---
+++ src/modifier/log_modifier.hpp
@@ -0,0 +1,45 @@
+/*
+**  Xbox360 USB Gamepad Userspace Driver
+**  Copyright (C) 2011 Ingo Ruhnke <grumbel@gmx.de>
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef HEADER_XBOXDRV_MODIFIER_LOG_MODIFIER_HPP
+#define HEADER_XBOXDRV_MODIFIER_LOG_MODIFIER_HPP
+
+#include "modifier.hpp"
+
+class LogModifier : public Modifier
+{
+public:
+  static LogModifier* from_string(const std::string& value);
+
+public:
+  LogModifier(const std::string& value);
+  void update(int msec_delta, ControllerMessage& msg);
+
+  std::string str() const;
+
+private:
+  std::string m_prefix;
+
+private:
+  LogModifier(const LogModifier&);
+  LogModifier& operator=(const LogModifier&);
+};
+
+#endif
+
+/* EOF */
