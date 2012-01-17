b4982adaaa66aa9cfe83d7a89ba2619a5a815839
Ingo Ruhnke <grumbel@gmail.com>
2012-01-17 13:46:02 +0100

    Made SymbolTable type insensitive

--- src/symbol_table.cpp
+++ src/symbol_table.cpp
@@ -20,8 +20,23 @@
 
 #include <stdexcept>
 #include <iostream>
+#include <ctype.h>
 
 #include "raise_exception.hpp"
+
+namespace {
+
+std::string tolower(const std::string& str)
+{
+  std::string ret = str;
+  for(std::string::size_type i = 0; i < ret.size(); ++i)
+  {
+    ret[i] = static_cast<char>(::tolower(ret[i]));
+  }
+  return ret;
+}
+
+} // namespace
 
 SymbolTable::SymbolTable() :
   m_int2name(),
@@ -30,9 +45,11 @@ SymbolTable::SymbolTable() :
 }
 
 int 
-SymbolTable::put(const std::string& name)
+SymbolTable::put(const std::string& name_)
 {
   //std::cout << "SymbolTable::put(" << name << ")" << std::endl;
+  std::string name = tolower(name_);
+
   int symbol = m_int2name.size();
   m_int2name.push_back(name);
   m_name2int[name] = symbol;
@@ -40,47 +57,55 @@ SymbolTable::put(const std::string& name)
 }
 
 int 
-SymbolTable::put(const std::string& name, 
+SymbolTable::put(const std::string& name_, 
                  const std::string& alias1)
 {
+  std::string name = tolower(name_);
+
   int symbol = m_int2name.size();
   m_int2name.push_back(name);
   m_name2int[name] = symbol;
-  m_name2int[alias1] = symbol;
+  m_name2int[tolower(alias1)] = symbol;
   return symbol;
 }
 
 int 
-SymbolTable::put(const std::string& name,
+SymbolTable::put(const std::string& name_,
                  const std::string& alias1,
                  const std::string& alias2)
 {
+  std::string name = tolower(name_);
+
   int symbol = m_int2name.size();
   m_int2name.push_back(name);
   m_name2int[name] = symbol;
-  m_name2int[alias1] = symbol;
-  m_name2int[alias2] = symbol;
+  m_name2int[tolower(alias1)] = symbol;
+  m_name2int[tolower(alias2)] = symbol;
   return symbol;
 }
 
 int 
-SymbolTable::put(const std::string& name,
+SymbolTable::put(const std::string& name_,
                  const std::string& alias1,
                  const std::string& alias2,
                  const std::string& alias3)
 {
+  std::string name = tolower(name_);
+
   int symbol = m_int2name.size();
   m_int2name.push_back(name);
   m_name2int[name] = symbol;
-  m_name2int[alias1] = symbol;
-  m_name2int[alias2] = symbol;
-  m_name2int[alias3] = symbol;
+  m_name2int[tolower(alias1)] = symbol;
+  m_name2int[tolower(alias2)] = symbol;
+  m_name2int[tolower(alias3)] = symbol;
   return symbol;
 }
 
 int 
-SymbolTable::getput(const std::string& name)
+SymbolTable::getput(const std::string& name_)
 {
+  std::string name = tolower(name_);
+
   std::map<std::string, int>::const_iterator it = m_name2int.find(name);
   if (it == m_name2int.end())
   {
@@ -93,8 +118,10 @@ SymbolTable::getput(const std::string& name)
 }
 
 int 
-SymbolTable::get(const std::string& name) const
+SymbolTable::get(const std::string& name_) const
 {
+  std::string name = tolower(name_);
+
   std::map<std::string, int>::const_iterator it = m_name2int.find(name);
   if (it == m_name2int.end())
   {
