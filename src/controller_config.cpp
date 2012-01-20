9de37de31824b4da6f13673b7fe22ffe360dd998
Ingo Ruhnke <grumbel@gmail.com>
2012-01-20 21:57:39 +0100

    Disabled some currently non-working code

--- src/controller_config.cpp
+++ src/controller_config.cpp
@@ -48,6 +48,7 @@ ControllerConfig::ControllerConfig(UInput& uinput, int slot, bool extra_devices,
 
   if (opts.deadzone)
   {
+#if 0
     boost::shared_ptr<AxismapModifier> axismap(new AxismapModifier);
 
     XboxAxis axes[] = { XBOX_AXIS_X1,
@@ -65,6 +66,7 @@ ControllerConfig::ControllerConfig(UInput& uinput, int slot, bool extra_devices,
     }
 
     m_modifier.push_back(axismap);
+#endif
   }
 
   if (opts.deadzone_trigger)
