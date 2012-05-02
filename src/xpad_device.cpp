f16b961150e936244bda9d87fef8ab2d1e08048d
Ingo Ruhnke <grumbel@gmail.com>
2012-05-02 18:40:10 +0200

    Added Elite Glow, thanks to Old Macky <spaceporker@gmail.com>

--- src/xpad_device.cpp
+++ src/xpad_device.cpp
@@ -89,6 +89,7 @@ XPadDevice xpad_devices[] = {
   { GAMEPAD_XBOX360,          0x1689, 0xfd00, "Razer Onza" },  
   { GAMEPAD_XBOX360,          0x12ab, 0x0004, "DDR Universe 2 Mat" }, 
   { GAMEPAD_XBOX360,          0x15e4, 0x3f0a, "Xbox Airflo wired controller" },
+  { GAMEPAD_XBOX360,          0x24c6, 0x5300, "Power A Mini Pro Elite Glow" },
 
   { GAMEPAD_XBOX_MAT,         0x0738, 0x4540, "Mad Catz Beat Pad" },
   { GAMEPAD_XBOX_MAT,         0x0738, 0x6040, "Mad Catz Beat Pad Pro" },
