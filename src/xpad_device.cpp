537edad4e413dccac2890613c32f3faa8f9c6101
Ingo Ruhnke <grumbel@gmail.com>
2013-04-04 18:39:00 +0200

    Added "Batarang Xbox 360 controller", thanks to Shaun Martin <shaun@samsite.ca>

--- src/xpad_device.cpp
+++ src/xpad_device.cpp
@@ -89,6 +89,7 @@ XPadDevice xpad_devices[] = {
   { GAMEPAD_XBOX360,          0x1bad, 0xf901, "Gamestop Xbox 360 Controller" },
   { GAMEPAD_XBOX360,          0x1bad, 0xf903, "Tron Xbox 360 controller" },
   { GAMEPAD_XBOX360,          0x15e4, 0x3f00, "Power A Mini Pro Elite" },
+  { GAMEPAD_XBOX360,          0x15e4, 0x3f10, "Batarang Xbox 360 controller" },
   { GAMEPAD_XBOX360_WIRELESS, 0x045e, 0x0291, "Microsoft Xbox 360 Wireless Controller" }, // RF Module from the Xbox360
   { GAMEPAD_XBOX360_WIRELESS, 0x045e, 0x0719, "Microsoft Xbox 360 Wireless Controller (PC)" }, // official Wireless Receiver
   { GAMEPAD_XBOX360,          0x1689, 0xfd00, "Razer Onza" },  
