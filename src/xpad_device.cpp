16e1e621b603f0404588933f0f2e943697691374
Ingo Ruhnke <grumbel@gmail.com>
2013-04-04 18:46:29 +0200

    Added "Xbox Airflo wired controller", thanks to Robert Hurst <theflyingape@gmail.com>

--- src/xpad_device.cpp
+++ src/xpad_device.cpp
@@ -70,6 +70,7 @@ XPadDevice xpad_devices[] = {
   { GAMEPAD_XBOX360,          0x0f0d, 0x000d, "Hori Fighting Stick Ex2" },
   { GAMEPAD_XBOX360,          0x0f0d, 0x0016, "Hori Real Arcade Pro Ex" },
   { GAMEPAD_XBOX360,          0x24c6, 0x5501, "Hori Real Arcade Pro VX-SA" },
+  { GAMEPAD_XBOX360,          0x24c6, 0x5303, "Xbox Airflo wired controller" },
   { GAMEPAD_XBOX360,          0x162e, 0xbeef, "Joytech Neo-Se Take2" },
   { GAMEPAD_XBOX360,          0x046d, 0xc21d, "Logitech F310" },
   { GAMEPAD_XBOX360,          0x046d, 0xc21e, "Logitech F510" },
