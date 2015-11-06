2d70f3c814eb629bbaa6046ed55a89b3fcf3f9a5
Ingo Ruhnke <grumbel@gmail.com>
2015-11-06 16:42:06 +0100

    Spelling fix: Ctrl-c -> Ctrl-C
    Fixes #94

--- src/xboxdrv_main.cpp
+++ src/xboxdrv_main.cpp
@@ -191,11 +191,11 @@ XboxdrvMain::run()
 
       if (m_opts.silent)
       {
-        std::cout << "\nPress Ctrl-c to quit" << std::endl;
+        std::cout << "\nPress Ctrl-C to quit" << std::endl;
       }
       else
       {
-        std::cout << "\nPress Ctrl-c to quit, use '--silent' to suppress the event output" << std::endl;
+        std::cout << "\nPress Ctrl-C to quit, use '--silent' to suppress the event output" << std::endl;
       }
     }
 
