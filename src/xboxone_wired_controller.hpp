2c49b9c556c3495fcbd86a9d6f15e1112ff12876
Grzegorz Antoniak <ga@anadoxin.org>
2015-12-25 19:18:05 +0100

    Code style changes, added file comment.

--- src/xboxone_wired_controller.hpp
+++ src/xboxone_wired_controller.hpp
@@ -7,31 +7,29 @@
 
 struct XPadDevice;
 
-class XboxOneWiredController: public USBController {
+class XboxOneWiredController: public USBController
+{
 private:
-    XPadDevice* dev_type;
+  XPadDevice* dev_type;
 
-    int m_endpoint;
-    int m_interface;
-    uint8_t m_sequence;
-
-    int m_battery_status;
-    std::string m_serial;
-    bool sent_auth;
-    bool guide_button;
+  int endpoint;
+  int interface;
+  uint8_t sequence;
+  bool sent_auth;
+  bool guide_button;
 
 private:
-    bool parse_button_status(uint8_t* data, int len, XboxGenericMsg* omsg);
-    bool parse_ledbutton_status(uint8_t* data, int len, XboxGenericMsg* omsg);
-    bool parse_init_status(uint8_t* data, int len, XboxGenericMsg* omsg);
-    bool parse_auth_status(uint8_t* data, int len, XboxGenericMsg* omsg);
+  bool parse_button_status(uint8_t* data, int len, XboxGenericMsg* omsg);
+  bool parse_ledbutton_status(uint8_t* data, int len, XboxGenericMsg* omsg);
+  bool parse_init_status(uint8_t* data, int len, XboxGenericMsg* omsg);
+  bool parse_auth_status(uint8_t* data, int len, XboxGenericMsg* omsg);
 
 public:
-    XboxOneWiredController(libusb_device* usb, int controller_id, bool try_detach);
-    virtual ~XboxOneWiredController();
+  XboxOneWiredController(libusb_device* usb, int controller_id, bool try_detach);
+  virtual ~XboxOneWiredController();
 
-    bool parse(uint8_t* data, int len, XboxGenericMsg* msg_out);
-    void set_rumble_real(uint8_t left, uint8_t right);
-    void set_led_real(uint8_t status);
+  bool parse(uint8_t* data, int len, XboxGenericMsg* msg_out);
+  void set_rumble_real(uint8_t left, uint8_t right);
+  void set_led_real(uint8_t status);
 };
 
