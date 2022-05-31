662952a6d860e15dc1868943f500e09e9fef6916
Ingo Ruhnke <grumbel@gmail.com>
2022-05-31 13:30:06 +0200

    Update flake.nix to nixos-22.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Xbox360 USB Gamepad Userspace Driver";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
