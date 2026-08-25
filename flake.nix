d318facb9f648543a631381c42a10b1db0bf1495
Ingo Ruhnke <grumbel@gmail.com>
2026-08-25 07:19:39 +0000

    Squashed 'external/argpp/' changes from 9e1d54f..8cac52f
    8cac52f CMake: expose both include roots for in-tree consumers
    b52420a Fix 'system' has been renamed warning
    a9f9813 Update to nixos-25.11
    c796f95 Remove .gitlab-ci
    
    git-subtree-dir: external/argpp
    git-subtree-split: 8cac52fee6a3ad152659500e5ce6396c87a3c2eb

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Command Line Argument Parser for C++";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
@@ -13,10 +13,10 @@
   outputs = { self, nixpkgs, flake-utils, tinycmmc }:
     tinycmmc.lib.eachSystemWithPkgs (pkgs:
       {
-        packages = flake-utils.lib.flattenTree rec {
+        packages = rec {
           default = argpp;
           argpp = pkgs.callPackage ./argpp.nix {
-            tinycmmc = tinycmmc.packages.${pkgs.system}.default;
+            tinycmmc = tinycmmc.packages.${pkgs.stdenv.hostPlatform.system}.default;
           };
         };
       }
